40f12ea33c226ed79570d0c149db74587a4d7901
Monika Dranitsa <dranitsam@gmail.com>
2020-10-03 22:00:58 +0300

    init: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
+  if (array === undefined || array.length === 0) {
   return 0;
 }
 
+let temp = array[0];
+
+array.forEach(element => {
+  if (element < temp) {
+    temp = element;
+  }
+});
+
+return temp;
+}
+
+
 exports.max = function max (array) {
+  if (array === undefined || array.length === 0) {
   return 0;
+}
+
+let temp = array[0];
+
+array.forEach(element => { 
+  if (element > temp) {
+    temp = element;
+  }  
+});
+
+return temp;
 }
 
 exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0) {
   return 0;
+  }
+
+  let temp = 0;
+
+  array.forEach(element => {
+    temp = temp + element;
+  });
+  return temp/array.length;
 }
